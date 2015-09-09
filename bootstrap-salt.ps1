4ef7ce7a311b762c2f03bf324cac9e540ee7b78e
TheBigBear <TheBigBear@users.noreply.github.com>
2015-09-09 10:42:55 +0100

    URL of salt windows downloads has changed
    The new location of the salt windows downloads is 'https://repo.saltstack.com/windows'
    On a related note not all salt windows minions published adhere to the strict 'Salt-Minion-$version-$arch-Setup.exe' naming.
    
    Some have a sort of packaging version or pkg release number appended as '-$pkgver', most recently this was the case with Salt-Minion-2015.5.1-3-AMD64-Setup.exe

--- bootstrap-salt.ps1
+++ bootstrap-salt.ps1
@@ -40,7 +40,7 @@ is dynamically determined by the script.
 Bootstrap GitHub Project (script home) - https://github.com/saltstack/salt-windows-bootstrap
 Original Vagrant Provisioner Project -https://github.com/saltstack/salty-vagrant
 Vagrant Project (utilizes this script) - https://github.com/mitchellh/vagrant
-SaltStack Download Location - http://docs.saltstack.com/downloads/
+SaltStack Download Location - https://repo.saltstack.com/windows/
 #>
 [CmdletBinding()]
 Param(
@@ -102,7 +102,7 @@ If ([IntPtr]::Size -eq 4) {
 # Download minion setup file
 Write-Host -NoNewline "Downloading Salt minion installer Salt-Minion-$version-$arch-Setup.exe"
 $webclient = New-Object System.Net.WebClient
-$url = "https://docs.saltstack.com/downloads/Salt-Minion-$version-$arch-Setup.exe"
+$url = "https://repo.saltstack.com/windows/Salt-Minion-$version-$arch-Setup.exe"
 $file = "C:\tmp\salt.exe"
 $webclient.DownloadFile($url, $file)
 
